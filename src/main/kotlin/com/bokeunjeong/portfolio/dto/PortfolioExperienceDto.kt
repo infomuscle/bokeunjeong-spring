789ce575597999e99cf05be0ecaec1faabb4a6e6
infomuscle <infomuscle10@gmail.com>
2020-09-24 19:21:22 +0900

    Added Sampele DTO fields

--- src/main/kotlin/com/bokeunjeong/portfolio/dto/PortfolioExperienceDto.kt
+++ src/main/kotlin/com/bokeunjeong/portfolio/dto/PortfolioExperienceDto.kt
@@ -2,10 +2,21 @@ package com.bokeunjeong.portfolio.dto
 
 import lombok.Getter
 import lombok.Setter
+import java.util.*
 
 @Getter
 @Setter
 class PortfolioExperienceDto {
+
+
+    var title: String = ""
+    var grade: String = ""
+
+
+    var startDt: Date = Date()
+    var endDt: Date = Date()
+
+
 
 
 
--- src/main/kotlin/com/bokeunjeong/portfolio/dto/PortfolioProjectDto.kt
+++ src/main/kotlin/com/bokeunjeong/portfolio/dto/PortfolioProjectDto.kt
@@ -2,17 +2,18 @@ package com.bokeunjeong.portfolio.dto
 
 import lombok.Getter
 import lombok.Setter
+import java.util.*
 
 @Getter
 @Setter
 class PortfolioProjectDto {
 
+    var title: String = ""
+    var client: String = ""
+    var description: String = ""
 
-    var title: String?
-    
-
-
-
+    var startDt: Date = Date()
+    var endDt: Date = Date()
 
 
 }
