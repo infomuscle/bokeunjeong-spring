932ba6ac014172bd876a6f07379c686a3231aafc
infomuscle <infomuscle10@gmail.com>
2020-10-02 02:53:32 +0900

    Splited to Multiple Pages

--- src/main/kotlin/com/bokeunjeong/portfolio/controller/PortfolioController.kt
+++ src/main/kotlin/com/bokeunjeong/portfolio/controller/PortfolioController.kt
@@ -5,11 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping
 
 
 @Controller
-class PortfolioController{
+class PortfolioController {
 
     @GetMapping
     fun index(): String {
-
         return "index"
+    }
+
+    @GetMapping("/about")
+    fun about(): String {
+        return "about"
+    }
+
+    @GetMapping("/projects")
+    fun projects(): String {
+        return "projects"
+    }
+
+    @GetMapping("/contact")
+    fun contact(): String {
+        return "contact"
     }
 }
