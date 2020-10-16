63319c1dee0c3de7dfb96f0bc243972538d9f1be
infomuscle <infomuscle10@gmail.com>
2020-10-16 16:47:28 +0900

    Added Kafka Produce Test Controller

--- src/main/kotlin/com/bokeunjeong/portfolio/kafka/producer/MailKafkaProducer.kt
+++ src/main/kotlin/com/bokeunjeong/portfolio/kafka/producer/MailKafkaProducer.kt
@@ -10,34 +10,34 @@ import javax.websocket.SendResult
 @Service
 class MailKafkaProducer {
 
-//    var prop: Properties = Properties()
-//    lateinit var producer: KafkaProducer<String, String>
-//
-//
-//    init {
-//        prop.put("bootstrap.servers", "localhost:9092");
-//        prop.put("acks", "all");
-//        prop.put("block.on.buffer.full", "true");
-//        prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//
-//        producer = KafkaProducer<String, String>(prop)
-//    }
-//
-//    fun produce(topic: String, msg: String): String {
-//
-//        this.sendMessageAsync(topic, msg)
-//
-//        return msg
-//    }
-//
-//    private fun sendMessageAsync(topic: String, msg: String): String {
-//
-//        var record: ProducerRecord<String, String> = ProducerRecord<String, String>(topic, msg)
-////        var future: ListenableFuture<SendResult<String, String>>
-//        producer.send(record)
-//
-//        return msg;
-//    }
+    var prop: Properties = Properties()
+    lateinit var producer: KafkaProducer<String, String>
+
+
+    init {
+        prop.put("bootstrap.servers", "localhost:9092");
+        prop.put("acks", "all");
+        prop.put("block.on.buffer.full", "true");
+        prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        producer = KafkaProducer<String, String>(prop)
+    }
+
+    fun produce(topic: String, msg: String): String {
+
+        this.sendMessageAsync(topic, msg)
+
+        return msg
+    }
+
+    private fun sendMessageAsync(topic: String, msg: String): String {
+
+        var record: ProducerRecord<String, String> = ProducerRecord<String, String>(topic, msg)
+//        var future: ListenableFuture<SendResult<String, String>>
+        producer.send(record)
+
+        return msg;
+    }
 }
 
--- src/test/kotlin/com/bokeunjeong/portfolio/PortfolioApplicationTests.kt
+++ src/test/kotlin/com/bokeunjeong/portfolio/PortfolioApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.bokeunjeong.portfolio
 
+import com.bokeunjeong.portfolio.kafka.producer.MailKafkaProducer
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,18 @@ class PortfolioApplicationTests {
 
     @Test
     fun contextLoads() {
+    }
+
+    @Test
+    fun testKafkaProduce() {
+
+        var topic = "test"
+        var msg = "THIS IS TEST MESSAGE"
+
+        var producer = MailKafkaProducer()
+        producer.produce(topic, msg)
+        println("TESTTESTTEST" + msg)
+
     }
 
 }
