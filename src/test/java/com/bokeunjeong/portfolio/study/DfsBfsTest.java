a92f77bb2e6f26f236ae184222c7379e57453fa1
infomuscle <infomuscle10@gmail.com>
2021-10-11 20:52:46 +0900

    Test Search Order for List Graph and Set Graph

--- src/test/java/com/bokeunjeong/portfolio/study/DfsBfsTest.java
+++ src/test/java/com/bokeunjeong/portfolio/study/DfsBfsTest.java
@@ -11,31 +11,57 @@ public class DfsBfsTest {
 
 
     @Test
-    public void dfsTest() throws Exception {
+    public void testDfsBfsOfSet() throws Exception {
 
-        Map<String, Set<String>> graph = new HashMap<>();
-        graph.put("A", Set.of("B"));
-        graph.put("B", Set.of("A", "C", "H"));
-        graph.put("C", Set.of("B", "D"));
-        graph.put("D", Set.of("C", "E", "G"));
-        graph.put("E", Set.of("D", "F"));
-        graph.put("F", Set.of("E"));
-        graph.put("G", Set.of("D"));
-        graph.put("H", Set.of("B", "I", "J", "M"));
-        graph.put("I", Set.of("H"));
-        graph.put("J", Set.of("H", "K"));
-        graph.put("K", Set.of("J", "L"));
-        graph.put("L", Set.of("K"));
-        graph.put("M", Set.of("M"));
+        Map<String, Set<String>> setGraph = new HashMap<>();
+        setGraph.put("A", Set.of("B"));
+        setGraph.put("B", Set.of("A", "C", "H"));
+        setGraph.put("C", Set.of("B", "D"));
+        setGraph.put("D", Set.of("C", "E", "G"));
+        setGraph.put("E", Set.of("D", "F"));
+        setGraph.put("F", Set.of("E"));
+        setGraph.put("G", Set.of("D"));
+        setGraph.put("H", Set.of("B", "I", "J", "M"));
+        setGraph.put("I", Set.of("H"));
+        setGraph.put("J", Set.of("H", "K"));
+        setGraph.put("K", Set.of("J", "L"));
+        setGraph.put("L", Set.of("K"));
+        setGraph.put("M", Set.of("H"));
 
-        List<String> dfsVisit = dfs(graph, "A");
+        List<String> dfsVisit = setDfs(setGraph, "A");
         System.out.println(dfsVisit);
 
-        List<String> bfsVisit = bfs(graph, "A");
+        List<String> bfsVisit = setBfs(setGraph, "A");
         System.out.println(bfsVisit);
     }
 
-    private List<String> dfs(Map<String, Set<String>> graph, String start) {
+
+    @Test
+    public void testDfsBfsOfList() throws Exception {
+
+        Map<String, List<String>> listGraph = new HashMap<>();
+        listGraph.put("A", List.of("B"));
+        listGraph.put("B", List.of("A", "C", "H"));
+        listGraph.put("C", List.of("B", "D"));
+        listGraph.put("D", List.of("C", "E", "G"));
+        listGraph.put("E", List.of("D", "F"));
+        listGraph.put("F", List.of("E"));
+        listGraph.put("G", List.of("D"));
+        listGraph.put("H", List.of("B", "I", "J", "M"));
+        listGraph.put("I", List.of("H"));
+        listGraph.put("J", List.of("H", "K"));
+        listGraph.put("K", List.of("J", "L"));
+        listGraph.put("L", List.of("K"));
+        listGraph.put("M", List.of("H"));
+
+        List<String> dfsVisit = listDfs(listGraph, "A");
+        System.out.println(dfsVisit);
+
+        List<String> bfsVisit = listBfs(listGraph, "A");
+        System.out.println(bfsVisit);
+    }
+
+    private List<String> setDfs(Map<String, Set<String>> graph, String start) {
 
         List<String> visited = new ArrayList<>();
         Stack<String> stack = new Stack();
@@ -53,7 +79,44 @@ public class DfsBfsTest {
         return visited;
     }
 
-    private List<String> bfs(Map<String, Set<String>> graph, String start) {
+    private List<String> setBfs(Map<String, Set<String>> graph, String start) {
+
+        List<String> visited = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            String node = queue.poll();
+            System.out.println(node);
+            if (!visited.contains(node)) {
+                visited.add(node);
+                queue.addAll(graph.get(node).stream().filter(n -> !visited.contains(n)).collect(Collectors.toList()));
+            }
+        }
+
+        return visited;
+
+    }
+
+    private List<String> listDfs(Map<String, List<String>> graph, String start) {
+
+        List<String> visited = new ArrayList<>();
+        Stack<String> stack = new Stack();
+        stack.add(start);
+
+        while (!stack.isEmpty()) {
+            String node = stack.pop();
+            System.out.println(node);
+            if (!visited.contains(node)) {
+                visited.add(node);
+                stack.addAll(graph.get(node).stream().filter(n -> !visited.contains(n)).sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
+            }
+        }
+
+        return visited;
+    }
+
+    private List<String> listBfs(Map<String, List<String>> graph, String start) {
 
         List<String> visited = new ArrayList<>();
         Queue<String> queue = new LinkedList<>();
