138455ed943292e22195a302b5a7a607c9b93e95
infomuscle <infomuscle10@gmail.com>
2022-10-08 22:57:47 +0900

    return error status on exception

--- src/main/java/com/bokeunjeong/portfolio/controller/PortfolioApiControllerAdvice.java
+++ src/main/java/com/bokeunjeong/portfolio/controller/PortfolioApiControllerAdvice.java
@@ -1,16 +1,19 @@
 package com.bokeunjeong.portfolio.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
 
 @Slf4j
 @RestControllerAdvice("com.bokeunjeong.portfolio.controller")
 public class PortfolioApiControllerAdvice {
     @ExceptionHandler
-    public ResponseEntity handleException(Exception e) {
+    public ResponseEntity handleException(Exception e) throws IOException {
         log.info(e.getMessage(), e);
-        return ResponseEntity.ok(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
