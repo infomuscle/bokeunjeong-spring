12d2e4819803eab4207c2fefdd50c92befe6c497
infomuscle <infomuscle10@gmail.com>
2022-12-09 02:12:53 +0900

    sample code for blog

--- src/test/java/com/bokeunjeong/practice/pattern/strategy/StrategyTest.java
+++ src/test/java/com/bokeunjeong/practice/pattern/strategy/StrategyTest.java
@@ -20,4 +20,116 @@ public class StrategyTest {
         model.setFlyBehavior(new FlyRocketPowered());
         model.performFly();
     }
+
+    @Test
+    void test2() {
+
+        System.out.println("Fighter를 생성합니다.");
+        Player player1 = new Fighter();
+        player1.move();
+        player1.attack();
+        player1.sit();
+
+        System.out.println();
+
+        System.out.println("ThiefWarrior를 생성합니다.");
+        Player player2 = new AssasinFighter();
+        player2.move();
+        ((AssasinFighter) player2).switchMode();
+        player2.move();
+    }
+
+    public abstract class Player {
+
+        MoveBehavior moveBehavior;
+        AttackBehavior attackBehavior;
+
+        public void move() {
+            moveBehavior.performMove();
+        }
+
+        public void attack() {
+            attackBehavior.performAttack();
+        }
+
+        public void sit() {
+            System.out.println("자리에 앉아 체력을 회복합니다.");
+        }
+    }
+
+
+    public class Fighter extends Player {
+        public Fighter() {
+            this.moveBehavior = new WarriorMoveBehavior();
+            this.attackBehavior = new FighterAttackBehavior();
+        }
+    }
+
+
+    public class AssasinFighter extends Player {
+        public AssasinFighter() {
+            this.moveBehavior = new WarriorMoveBehavior();
+            this.attackBehavior = new FighterAttackBehavior();
+        }
+
+        public void switchMode() {
+            System.out.println("모드를 전환합니다.");
+            this.moveBehavior = (this.moveBehavior instanceof WarriorMoveBehavior) ? new ThieifMoveBehavior() : new WarriorMoveBehavior();
+            this.attackBehavior = (this.attackBehavior instanceof FighterAttackBehavior) ? new AssasinAttackBehavior() : new FighterAttackBehavior();
+        }
+    }
+
+
+
+    public interface MoveBehavior {
+        void performMove();
+    }
+
+
+    public class WarriorMoveBehavior implements MoveBehavior {
+        public void performMove() {
+            System.out.println("쿵쾅쿵쾅 움직입니다.");
+        }
+    }
+
+
+    public class MagicianMoveBehavior implements MoveBehavior {
+        public void performMove() {
+            System.out.println("스르르륵 움직입니다.");
+        }
+    }
+
+
+    public class ThieifMoveBehavior implements MoveBehavior {
+        public void performMove() {
+            System.out.println("슉슉슈슉 움직입니다.");
+        }
+    }
+
+
+    public interface AttackBehavior {
+        void performAttack();
+    }
+
+
+    public class FighterAttackBehavior implements AttackBehavior {
+        public void performAttack() {
+            System.out.println("칼로 벱니다.");
+        }
+    }
+
+
+    public class SpearmanAttackBehavior implements AttackBehavior {
+        public void performAttack() {
+            System.out.println("창으로 찌릅니다.");
+        }
+    }
+
+
+    public class AssasinAttackBehavior implements AttackBehavior {
+        public void performAttack() {
+            System.out.println("표창을 던집니다.");
+        }
+    }
+
 }
