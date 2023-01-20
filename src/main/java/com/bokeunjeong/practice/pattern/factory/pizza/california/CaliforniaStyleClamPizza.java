66c56c55862f7f90831bc9e684c8c049198079b8
infomuscle <infomuscle10@gmail.com>
2023-01-20 16:25:07 +0900

    factory patter(simplified pizza cases)

--- src/main/java/com/bokeunjeong/practice/pattern/factory/pizza/california/CaliforniaStyleClamPizza.java
+++
@@ -1,7 +0,0 @@
-package com.bokeunjeong.practice.pattern.factory.pizza.california;
-
-import com.bokeunjeong.practice.pattern.factory.pizza.Pizza;
-
-public class CaliforniaStyleClamPizza extends Pizza {
-
-}
--- src/main/java/com/bokeunjeong/practice/pattern/factory/pizza/california/CaliforniaStyleVeggiePizza.java
+++
@@ -1,6 +0,0 @@
-package com.bokeunjeong.practice.pattern.factory.pizza.california;
-
-import com.bokeunjeong.practice.pattern.factory.pizza.Pizza;
-
-public class CaliforniaStyleVeggiePizza extends Pizza {
-}
